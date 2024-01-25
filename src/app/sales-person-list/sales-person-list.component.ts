d6e450133eb324e582c508a6e095491dd75afefb
Myron Holmes <1dequanh@gmail.com>
2024-01-24 23:07:54 -0500

    new angular component created

---
+++ src/app/sales-person-list/sales-person-list.component.ts
@@ -0,0 +1,20 @@
+// Create new componant called Sales Person List: ng generate component sales-person-list       
+import { Component } from '@angular/core';
+import { EmailValidator } from '@angular/forms';
+import { SalesPerson } from './sales-person';
+
+@Component({
+  selector: 'app-sales-person-list',
+  templateUrl: './sales-person-list.component.html',
+  styleUrl: './sales-person-list.component.css'
+})
+export class SalesPersonListComponent {
+
+  // create array of objects
+  salesPersonList: SalesPerson[]= [
+    new SalesPerson("John", "Doe", "johndoe@email.com", 50000),
+    new SalesPerson("Jane", "Doe", "janedoe@email.com", 40000),
+    new SalesPerson("Jhonny", "Appleseed", "japple@email.com", 30000),
+    new SalesPerson("Harry", "Potter", "hp@gmail.com", 9000)
+  ]
+}
---
+++ src/app/sales-person-list/sales-person.ts
@@ -0,0 +1,6 @@
+// Create new SalesPerson class: ng generate class sales-person-list/SalesPerson
+export class SalesPerson {
+    constructor(public firstName: string, public lastName: string, public email: string, public salesVolume: number){
+        
+    }
+}
